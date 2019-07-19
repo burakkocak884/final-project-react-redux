846f58466159773ac8b562f8c22abf8e8b47d658
Burak Kocak <burakkocak884@ghotmail.com>
2019-07-19 17:03:57 -0400

    first push

---
+++ wheeler_finder_api/app/controllers/vehicles_controller.rb
@@ -0,0 +1,21 @@
+class VehiclesController < ApplicationController
+
+	def index
+    @vehicles = Vehicle.all
+    render json: @vehicles
+	end
+
+
+
+	def show
+
+
+	@vehicle = Vehicle.find_by(id: params[:id])
+	render json: @vehicle
+
+
+	end
+	def destroy
+
+	end
+end
